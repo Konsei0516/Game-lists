e47535920ece13007d51cd14ecf33a6eadffe551
Konsei0516 <masaya19960516@gmail.com>
2020-05-10 15:41:27 +0900

    楽天商品検索
    楽天APIによる商品検索機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,10 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def rakuten_search
+    if params[:keyword]
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword],hits: 10)
+    end
+  end
 end
---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,3 @@
+RakutenWebService.configure do |c|
+  c.application_id = '1020674377474477432'
+end
