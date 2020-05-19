0e4b229f578030435f54fcc4050fe99fdbf672ad
Konsei0516 <masaya19960516@gmail.com>
2020-05-19 14:17:10 +0900

    投稿のためのモデル作成
    reviewモデルと紐づくimageモデルの作成。
    仮のviewを作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,20 @@ class ReviewsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+
+  def destroy
+  end
+
   def rakuten_search
     if params[:keyword]
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword],hits: 10)
