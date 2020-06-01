4320596afba227b56a8521de4e708ce212eedbba
Konsei0516 <masaya19960516@gmail.com>
2020-06-01 21:41:18 +0900

    お気に入り一覧を取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id:params[:id])
+    @reviews = @user.reviews.order('created_at DESC').page(params[:page]).per(6)
+  end
+
+  def like
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id).page(params[:page]).per(6)
   end
 end
