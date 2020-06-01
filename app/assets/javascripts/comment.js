9aee21137d69beda8b99d6841f4b849e9ca1a2ff
Konsei0516 <masaya19960516@gmail.com>
2020-06-01 10:30:45 +0900

    検索フォームの調整

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,16 +1,18 @@
-$(function() {
-  let overlay      = $('.overlay');
-  let commentBtn   = $('.btn-box__comment-btn');
-  let commentClose = $('.comment-box__btn--close');
-  let commentBox   = $('.comment-box');
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    let overlay      = $('.overlay');
+    let commentBtn   = $('.btn-box__comment-btn');
+    let commentClose = $('.comment-box__btn--close');
+    let commentBox   = $('.comment-box');
 
   commentBtn.click(function() {
     overlay.fadeIn();
     commentBox.fadeIn();
   });
 
-  commentClose.click(function() {
-    overlay.fadeOut();
-    commentBox.fadeOut();
+    commentClose.click(function() {
+      overlay.fadeOut();
+      commentBox.fadeOut();
+    });
   });
-});
+})
