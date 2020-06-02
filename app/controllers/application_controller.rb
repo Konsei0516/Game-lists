45bdb71c7a6ffc7fefb8b169390f1d597dccac35
Konsei0516 <masaya19960516@gmail.com>
2020-06-02 09:57:07 +0900

    切り替えボタン
    お気に入りとレビュー一覧をプロフィールに表示。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:PSN_id,:friend_code])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile,:PSN_id,:friend_code])
   end
