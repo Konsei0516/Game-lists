9d7308d3f6de08b5f07d3cbcc41bcb957335afab
Konsei0516 <masaya19960516@gmail.com>
2020-06-03 13:08:20 +0900

    レビューの編集１
    編集時にプレビューを表示

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -19,6 +19,27 @@ $(document).on('turbolinks:load', function(){
       return html;
     }
 
+    if (window.location.href.match(/\/reviews\/\d+\/edit/)){
+      //登録済み画像のプレビュー表示欄の要素を取得する
+      var prevContent = $('.label-content').prev();
+      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+      $('.label-content').css('width', labelWidth);
+      //プレビューにidを追加
+      $('.preview-box').each(function(index, box){
+        $(box).attr('id', `preview-box__${index}`);
+      })
+      //削除ボタンにidを追加
+      $('.delete-box').each(function(index, box){
+        $(box).attr('id', `delete_btn_${index}`);
+      })
+      var count = $('.preview-box').length;
+      //プレビューが5あるときは、投稿ボックスを消しておく
+      if (count == 5) {
+        $('.label-content').hide();
+      }
+    }
+
+
     // ラベルのwidth操作
     function setLabel() {
       //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
@@ -72,25 +93,42 @@ $(document).on('turbolinks:load', function(){
     $(document).on('click', '.delete-box', function() {
       var count = $('.preview-box').length;
       setLabel(count);
-      //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //取得したidに該当するプレビューを削除
       $(`#preview-box__${id}`).remove();
-      console.log("new")
-      //フォームの中身を削除 
-      $(`#review_images_attributes_${id}_image`).val("");
 
-      //削除時のラベル操作
-      var count = $('.preview-box').length;
-      //5個めが消されたらラベルを表示
-      if (count == 4) {
-        $('.label-content').show();
-      }
-      setLabel(count);
+      //新規登録時と編集時の場合分け==========================================================
 
-      if(id < 5){
-        //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
-        $('.label-box').attr({id: `label-box--${id}`,for: `review_images_attributes_${id}_image`});
+      //新規投稿時
+      //削除用チェックボックスの有無で判定
+      if ($(`#review_images_attributes_${id}__destroy`).length == 0) {
+        //フォームの中身を削除 
+        $(`#review_images_attributes_${id}_image`).val("");
+        var count = $('.preview-box').length;
+        //5個めが消されたらラベルを表示
+        if (count == 4) {
+          $('.label-content').show();
+        }
+        setLabel(count);
+        if(id < 5){
+          $('.label-box').attr({id: `label-box--${id}`,for: `review_images_attributes_${id}_image`});
+
+        }
+      } else {
+
+        //投稿編集時
+        $(`#review_images_attributes_${id}__destroy`).prop('checked',true);
+        //5個めが消されたらラベルを表示
+        if (count == 4) {
+          $('.label-content').show();
+        }
+
+        //ラベルのwidth操作
+        setLabel();
+        //ラベルのidとforの値を変更
+        //削除したプレビューのidによって、ラベルのidを変更する
+        if(id < 5){
+          $('.label-box').attr({id: `label-box--${id}`,for: `review_images_attributes_${id}_image`});
+        }
       }
     });
   });
